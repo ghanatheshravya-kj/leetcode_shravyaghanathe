a584c70cb63ba35dae7ba2fbc43237dd3cabeb00
Ghanathe Shravya <ghanatheshravya@gmail.com>
2026-08-14 11:14:45 +0530

    Time: 1 ms (98.02%) | Memory: 42.8 MB (8.52%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int mySqrt(int x) {
         if (x < 2) {
             return x;
@@ -12,4 +12,26 @@ class Solution {
 
         return x;
     }
+}*/
+class Solution {
+    public int mySqrt(int x)
+    {
+
+
+    if (x < 2) 
+      return x;
+        int left = 1, right = x / 2, ans = 0;
+        while (left <= right
+        ) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x)
+             {
+                ans = mid;        // mid is a valid candidate
+                left = mid + 1;   // try to find a bigger one
+            } else {
+                right = mid - 1;  // mid^2 too big, move left
+            }
+        }
+        return ans;
+}
 }
