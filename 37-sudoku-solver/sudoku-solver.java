23ceb30d0cc0c75c5c78a87cf25bfe4c52bf2880
Ghanathe Shravya <ghanatheshravya@gmail.com>
2026-09-07 21:59:19 +0530

    Time: 91 ms (94.35%) | Memory: 43.1 MB (23.68%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,5 +1,17 @@
 class Solution {
+    boolean[][] row = new boolean[9][9];
+    boolean[][] col = new boolean[9][9];
+    boolean[][] box = new boolean[9][9];
+
     public void solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+                if (board[i][j] != '.') {
+                    int n = board[i][j] - '1';
+                    int b = (i / 3) * 3 + j / 3;
+                    row[i][n] = col[j][n] = box[b][n] = true;
+                }
+
         solve(board);
     }
 
@@ -7,25 +19,22 @@ class Solution {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (b[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (valid(b, i, j, c)) {
-                            b[i][j] = c;
+                    int boxId = (i / 3) * 3 + j / 3;
+
+                    for (int n = 0; n < 9; n++) {
+                        if (!row[i][n] && !col[j][n] && !box[boxId][n]) {
+                            b[i][j] = (char) (n + '1');
+                            row[i][n] = col[j][n] = box[boxId][n] = true;
+
                             if (solve(b)) return true;
+
                             b[i][j] = '.';
+                            row[i][n] = col[j][n] = box[boxId][n] = false;
                         }
                     }
                     return false;
                 }
             }
-        }
-        return true;
-    }
-
-    boolean valid(char[][] b, int r, int c, char x) {
-        for (int i = 0; i < 9; i++) {
-            if (b[r][i] == x || b[i][c] == x ||
-                b[r / 3 * 3 + i / 3][c / 3 * 3 + i % 3] == x)
-                return false;
         }
         return true;
     }
