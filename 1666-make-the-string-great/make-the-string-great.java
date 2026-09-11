6092d185ca6e6f1356fb9037b121c3bb5c1241c4
Ghanathe Shravya <ghanatheshravya@gmail.com>
2026-09-11 14:35:58 +0530

    Time: 3 ms (64.09%) | Memory: 44.3 MB (11.49%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String makeGood(String s) {
+        
+        Stack<Character> stack= new Stack<>();
+         for (char ch : s.toCharArray()) {
+            if(!stack.isEmpty() && (Math.abs(stack.peek() - ch)==32))
+            {
+                stack.pop();
+            }
+            else{
+                stack.push(ch);
+            }
+         }
+         String result="";
+         while(!stack.isEmpty())
+         {
+            result=stack.pop()+result;
+         }
+         return result;
+            
+    }
+}
